6585a4c5a51a9ad9bfcfe7c2d80ecfb7235f201c
Simon Jeppesen <simonjeppesen93@gmail.com>
2022-09-04 17:47:14 +0200

    black formatting

--- src/chess/utils.py
+++ src/chess/utils.py
@@ -7,17 +7,29 @@ Missing utils:
 import chess
 from textwrap import wrap
 
-def get_binary_from_bb(bb: int) -> str:
-    return '{:064b}'.format(bb)
+
+def get_binary_string_from_bb(bb: int) -> str:
+    return "{:064b}".format(bb)
+
 
 def print_bitboard(bb: int):
-    print('\n'.join([' '.join(wrap(line[::-1], 1)) for line in wrap(get_binary_from_bb(bb), 8)]))
+    print(
+        "\n".join(
+            [
+                " ".join(wrap(line[::-1], 1))
+                for line in wrap(get_binary_string_from_bb(bb), 8)
+            ]
+        )
+    )
+
 
 def get_bb_from_binary(bb: int):
     pass
 
+
 def get_square_int_from_bb(bb: int):
     return chess.BB_SQUARES.index(bb)
+
 
 def get_file_from_bb(bb: int):
     if bb & chess.BB_FILE_A != 0: return chess.BB_FILE_A
@@ -30,6 +42,7 @@ def get_file_from_bb(bb: int):
     if bb & chess.BB_FILE_H != 0: return chess.BB_FILE_H
     return 0
 
+
 def get_rank_from_bb(bb: int):
     if bb & chess.BB_RANK_1 != 0: return chess.BB_RANK_1
     if bb & chess.BB_RANK_2 != 0: return chess.BB_RANK_2
@@ -41,12 +54,12 @@ def get_rank_from_bb(bb: int):
     if bb & chess.BB_RANK_8 != 0: return chess.BB_RANK_8
     return 0
 
-    
+
 def get_individual_ones_in_bb(bb_pieces: chess.Bitboard):
     """
     Generates 1 bit bitboards from the decomposition of a bitboard
     """
     while bb_pieces != 0:
-        most_sig_bit = 2**(bb_pieces.bit_length()-1)
+        most_sig_bit = 2 ** (bb_pieces.bit_length() - 1)
         yield most_sig_bit
         bb_pieces ^= most_sig_bit
