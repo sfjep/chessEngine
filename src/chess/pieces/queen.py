a5e931bf82b0143d657f77959c5ba194720e1403
Simon Jeppesen <simonjeppesen93@gmail.com>
2022-09-04 17:49:07 +0200

    rename target moves to moves

--- src/chess/pieces/queen.py
+++ src/chess/pieces/queen.py
@@ -18,7 +18,7 @@ class Queen(Piece):
                 Moves.move_down_left_diagonal(bb_square) |
                 Moves.move_down_right_diagonal(bb_square) |
                 Moves.move_up_left_diagonal(bb_square) |
-                Moves.move_up_right_diagonal(bb_square) 
+                Moves.move_up_right_diagonal(bb_square)
             )
 
         return moves_lookup
@@ -30,7 +30,7 @@ class Queen(Piece):
         attack_actions = []
 
         for current_piece_position in get_individual_ones_in_bb(self.bb):
-            target_moves = chess.BB_EMPTY
+            moves = chess.BB_EMPTY
             attack_moves = chess.BB_EMPTY
             move_generator = [
                 Moves.move_up,
@@ -40,7 +40,7 @@ class Queen(Piece):
                 Moves.move_down_left_diagonal,
                 Moves.move_down_right_diagonal,
                 Moves.move_up_left_diagonal,
-                Moves.move_up_right_diagonal 
+                Moves.move_up_right_diagonal
             ]
             for next_move in move_generator:
                 continue_in_direction = True
@@ -52,12 +52,12 @@ class Queen(Piece):
                     elif next_square & player_occupied:
                         continue_in_direction = False
                     else:
-                        target_moves |= next_square
-                        if target_moves & opponent_occupied:
+                        moves |= next_square
+                        if moves & opponent_occupied:
                             attack_moves |= next_square
                             continue_in_direction = False
 
-            queen_actions += Action.generate_actions(target_moves, chess.QUEEN, current_piece_position)
+            queen_actions += Action.generate_actions(moves, chess.QUEEN, current_piece_position)
             attack_actions += Action.generate_actions(attack_moves, chess.QUEEN, current_piece_position)
 
         return queen_actions, attack_actions
