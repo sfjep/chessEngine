005016644d17f7cfe826ce96613844c6b8c10cec
Simon Jeppesen <simonjeppesen93@gmail.com>
2022-09-04 17:49:53 +0200

    add field types

--- src/chess/pieces/piece.py
+++ src/chess/pieces/piece.py
@@ -1,13 +1,17 @@
 from abc import ABC, abstractmethod
+import chess
 
 class Piece(ABC):
-    
+
+    bb: chess.Bitboard
+    color: bool
+    piece_type: int
+
     def __init__(self, bb, color, piece_type):
         self.bb = bb
         self.color = color
         self.piece_type = piece_type
-    
+
     @abstractmethod
     def generate_move_lookup(self):
         pass
-    
