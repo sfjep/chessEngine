af10ece39d74c7710103fc5fe0ec0be5c9db8fb0
Simon Jeppesen <simonjeppesen93@gmail.com>
2023-02-27 21:07:23 +0100

    add logic for clicking a piece

--- src/chess/gui.py
+++ src/chess/gui.py
@@ -24,10 +24,18 @@ class GUI:
 
 	def run(self, board):
 		running = True
+		square_selected = () # no square is selected, keep track of the last click of the user (tuple: (row, col))
+		player_clicks = [] # keep track of player clicks (two tuples: [(6, 4), (4, 4)])
 		while running:
 			for e in p.event.get():
 				if e.type==p.QUIT:
 					running=False
+				elif e.type == p.MOUSEBUTTONDOWN:
+					location = p.mouse.get_pos() # (x,y) location of mouse
+					column = location[0] // self.SQUARE_SIZE
+					row = location[1] // self.SQUARE_SIZE
+
+
 			self.draw_board()
 			self.draw_pieces(board)
 			self.clock.tick(self.MAX_FPS)
