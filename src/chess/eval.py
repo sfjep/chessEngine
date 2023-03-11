6ec0a6bd4412b4d8ed14d8470f844591ee7562c4
Simon Jeppesen <simonjeppesen93@gmail.com>
2023-03-11 18:43:52 +0100

    add eval class

---
+++ src/chess/eval.py
@@ -0,0 +1,9 @@
+from action import Action
+
+class Eval:
+	def __init__(self, actions: list):
+		self.actions = actions
+
+	def get_best_action(self) -> Action:
+		for action in self.actions:
+			pass
