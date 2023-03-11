58bdc3e1f56deb563f543914ca63039fa617cd8e
Simon Jeppesen <simonjeppesen93@gmail.com>
2023-03-11 23:42:26 +0100

    clean up init method

--- src/chess/board.py
+++ src/chess/board.py
@@ -27,13 +27,23 @@ class Board:
     pieces: list
 
     def __init__(self, fen_board: str = None):
+        self._create_instance_of_each_piece()
+
+        if not fen_board:
+            self._set_board_to_start_position()
+        else:
+            self._set_board_from_fen(fen_board)
+
+        self._set_helper_bitboard()
+        self._get_board_chararray()
+
+    def _create_instance_of_each_piece(self):
         self.WP = Pieces.Pawn(chess.BB_EMPTY, chess.WHITE, chess.PAWN)
         self.WR = Pieces.Rook(chess.BB_EMPTY, chess.WHITE, chess.ROOK)
         self.WN = Pieces.Knight(chess.BB_EMPTY, chess.WHITE, chess.KNIGHT)
         self.WB = Pieces.Bishop(chess.BB_EMPTY, chess.WHITE, chess.BISHOP)
         self.WQ = Pieces.Queen(chess.BB_EMPTY, chess.WHITE, chess.QUEEN)
         self.WK = Pieces.King(chess.BB_EMPTY, chess.WHITE, chess.KING)
-
         self.BP = Pieces.Pawn(chess.BB_EMPTY, chess.BLACK, chess.PAWN)
         self.BR = Pieces.Rook(chess.BB_EMPTY, chess.BLACK, chess.ROOK)
         self.BN = Pieces.Knight(chess.BB_EMPTY, chess.BLACK, chess.KNIGHT)
@@ -41,56 +51,57 @@ class Board:
         self.BQ = Pieces.Queen(chess.BB_EMPTY, chess.BLACK, chess.QUEEN)
         self.BK = Pieces.King(chess.BB_EMPTY, chess.BLACK, chess.KING)
 
-        if not fen_board:  # starting board from initial game position
-            self.WP.bb = chess.BB_RANK_2
-            self.WR.bb = chess.BB_A1 | chess.BB_H1
-            self.WN.bb = chess.BB_B1 | chess.BB_G1
-            self.WB.bb = chess.BB_C1 | chess.BB_F1
-            self.WQ.bb = chess.BB_D1
-            self.WK.bb = chess.BB_E1
+    def _set_board_to_start_position(self):
+        self.WP.bb = chess.BB_RANK_2
+        self.WR.bb = chess.BB_A1 | chess.BB_H1
+        self.WN.bb = chess.BB_B1 | chess.BB_G1
+        self.WB.bb = chess.BB_C1 | chess.BB_F1
+        self.WQ.bb = chess.BB_D1
+        self.WK.bb = chess.BB_E1
 
-            self.BP.bb = chess.BB_RANK_7
-            self.BN.bb = chess.BB_B8 | chess.BB_G8
-            self.BR.bb = chess.BB_A8 | chess.BB_H8
-            self.BB.bb = chess.BB_C8 | chess.BB_F8
-            self.BQ.bb = chess.BB_D8
-            self.BK.bb = chess.BB_E8
+        self.BP.bb = chess.BB_RANK_7
+        self.BN.bb = chess.BB_B8 | chess.BB_G8
+        self.BR.bb = chess.BB_A8 | chess.BB_H8
+        self.BB.bb = chess.BB_C8 | chess.BB_F8
+        self.BQ.bb = chess.BB_D8
+        self.BK.bb = chess.BB_E8
 
-        else:
-            char_to_piece = {
-                "P": self.WP,
-                "R": self.WR,
-                "N": self.WN,
-                "B": self.WB,
-                "Q": self.WQ,
-                "K": self.WK,
-                "p": self.BP,
-                "r": self.BR,
-                "n": self.BN,
-                "b": self.BB,
-                "q": self.BQ,
-                "k": self.BK,
-            }
-            fen = fen_board.split("/")
-            if len(fen) != 8:
-                raise InvalidFenException
-            blank_spaces = "1234567"
-            # enumerating ranks from 7 to 0, as fen starts from black to white
-            for rank, rank_content in zip(range(7, -1, -1), fen):
-                if rank_content != "8":
-                    file_count = 0
-                    for char in rank_content:
-                        if char in blank_spaces:
-                            file_count += int(char)
-                        else:
-                            char_to_piece[char].bb |= chess.BB_SQUARES[
-                                rank * 8 + file_count
-                            ]
-                            file_count += 1
-                        if file_count > 8:
-                            raise InvalidFenException
 
-        # Helper bitboards
+    def _set_board_from_fen(self, fen_board: str):
+        char_to_piece = {
+            "P": self.WP,
+            "R": self.WR,
+            "N": self.WN,
+            "B": self.WB,
+            "Q": self.WQ,
+            "K": self.WK,
+            "p": self.BP,
+            "r": self.BR,
+            "n": self.BN,
+            "b": self.BB,
+            "q": self.BQ,
+            "k": self.BK,
+        }
+        fen = fen_board.split("/")
+        if len(fen) != 8:
+            raise InvalidFenException
+        blank_spaces = "1234567"
+        # enumerating ranks from 7 to 0, as fen starts from black to white
+        for rank, rank_content in zip(range(7, -1, -1), fen):
+            if rank_content != "8":
+                file_count = 0
+                for char in rank_content:
+                    if char in blank_spaces:
+                        file_count += int(char)
+                    else:
+                        char_to_piece[char].bb |= chess.BB_SQUARES[
+                            rank * 8 + file_count
+                        ]
+                        file_count += 1
+                    if file_count > 8:
+                        raise InvalidFenException
+
+    def _set_helper_bitboard(self):
         self.white_occupied = (
             self.WP.bb | self.WR.bb | self.WB.bb | self.WN.bb | self.WQ.bb | self.WK.bb
         )
@@ -98,12 +109,10 @@ class Board:
             self.BP.bb | self.BR.bb | self.BB.bb | self.BN.bb | self.BQ.bb | self.BK.bb
         )
         self.all_occupied = self.white_occupied | self.black_occupied
-
         self.white_pieces = [self.WP, self.WR, self.WB, self.WN, self.WQ, self.WK]
         self.black_pieces = [self.BP, self.BR, self.BB, self.BN, self.BQ, self.BK]
         self.pieces = self.white_pieces + self.black_pieces
 
-        self.get_board_chararray()
 
     @staticmethod
     def piece_squares(bb_pieces: Bitboard):
@@ -115,7 +124,7 @@ class Board:
             yield most_sig_bit
             bb_pieces ^= most_sig_bit
 
-    def get_board_chararray(self):
+    def _get_board_chararray(self):
         self.board_arr = np.full([8, 8], " ", dtype=object)
         for piece in self.pieces:
             for bb in get_individual_ones_in_bb(piece.bb):
