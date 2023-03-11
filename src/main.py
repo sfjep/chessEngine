c6c4d9df9290312be0a4efb50dd6d74090bc72bb
Simon Jeppesen <simonjeppesen93@gmail.com>
2023-03-11 23:43:06 +0100

    queen action test

--- src/main.py
+++ src/main.py
@@ -4,19 +4,20 @@ from chess.gui.gui import GUI
 
 
 def main():
-    state = State("r1bqkbnr/pp1ppppp/2n5/7P/1P1pP1Q1/8/PPP3PP/2B1KB1R w KQkq - 3 6")
-    gui = GUI()
-    gui.run(state)
+    state = State("rQbqkbnr/pp1ppppp/2n5/7P/1P1pP3/8/PPP3PP/2B1KB1R w KQkq - 3 6")
+    queen_actions, attack_actions = state.board.WQ.get_moves(
+        state.board.black_occupied,
+        state.board.white_occupied,
+    )
+    print("Queen actions: ", queen_actions)
+    print("Queen attacks: ", attack_actions)
+    # gui = GUI()
+    # gui.run(state)
 
 
 
 
     # print(Fen.get_fen_from_state(state))
-    queen_actions, attack_actions = state.board.WQ.get_moves(
-        state.board.black_occupied,
-        state.board.white_occupied,
-    )
-    print(queen_actions)
     # print(attack_actions)
     # state.board.print_board_from_board_obj()
 
