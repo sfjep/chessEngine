3fb308540c962e1ec9a07de854d7d804d180a190
Simon Jeppesen <simonjeppesen93@gmail.com>
2023-06-17 20:57:56 +0200

    add new game flow

--- src/chess/game.py
+++ src/chess/game.py
@@ -1,7 +1,7 @@
 from state import State
 
-def start_game():
-    s = State().get_initial_state()
+def start_game(fen=None):
+    s = State(fen) if fen else State().get_initial_state()
 
     while not s.is_checkmate():
         s.get_possible_actions()
--- src/main.py
+++ src/main.py
@@ -1,14 +1,16 @@
 from chess.state import State
 import pygame as p
 from chess.gui.gui import GUI
-
+from chess.game import start_game
 
 def main():
+    # start_game("rQbqk2r/pp1ppppp/8/2pPB2P/1P1pP3/8/PPP3PP/R1n1K1NR w KQkq c6 3 6")
     state = State("rQbqk2r/pp1ppppp/8/2pPB2P/1P1pP3/8/PPP3PP/R1n1K1NR w KQkq c6 3 6")
-    moves, attacks = state.get_possible_actions()
+    state.get_possible_actions()
 
-    print("Actions: ", moves)
-    print("Attacks: ", attacks)
+    print("Actions: ", state.moves)
+    print("Attacks: ", state.attacks)
+
     gui = GUI()
     gui.run(state)
 
