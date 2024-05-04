3bf692026e226bf5061ea8101ce0513cb7f81e03
sfjep <simonjeppesen93@gmail.com>
2024-05-04 15:30:43 +0200

    add perftTest
    Co-authored-by: victorianunezr <victorianunezr@users.noreply.github.com>

--- src/chess/search.py
+++ src/chess/search.py
@@ -1,5 +1,7 @@
 from collections import deque
 from chess.state import State
+from chess.state import Fen
+
 
 
 def search(s0: State, depth_cutoff: int):
@@ -22,12 +24,48 @@ def search(s0: State, depth_cutoff: int):
 
         result[depth] += len(s.possible_actions)
         node_count_per_level[depth] += 1
-        
+
         # only expand tree if depth < depth_cutoff
-        if depth < depth_cutoff: 
+        if depth < depth_cutoff:
             for action in s.possible_actions:
                 new_state = s.apply_action(action)
                 s.children.append(new_state)
                 queue.append(new_state)
 
     return result
+
+
+
+def perfTest(depth: int, state: State):
+    total_leaf_nodes = 0
+
+    for action in state.possible_actions:
+        leaf_nodes = 0
+        state.apply_action(action)
+
+        leaf_nodes = perft(depth-1, state)
+        total_leaf_nodes += leaf_nodes
+
+        print(f"Move: {action}, Leaf nodes: {leaf_nodes}")
+
+        state = state.undo_action()
+
+    print(f"Test complete. Total number of leaves: {total_leaf_nodes}")
+
+
+def perft(depth: int, state: State):
+    if depth == 1:
+        return len(state.possible_actions)
+    elif depth > 1:
+        count = 0
+
+        for action in state.possible_actions:
+            state.apply_action(action)
+            parent = state.undo_action()
+            count += perft(depth - 1, state)
+            state = parent
+
+        return count
+
+    else:
+        return 1
--- src/main.py
+++ src/main.py
@@ -1,19 +1,23 @@
 from chess.state import State
+from chess.search import perfTest
 import pygame as p
 from chess.gui.gui import GUI
 from chess.state import State
 
 def main():
+    state = State()
+    perfTest(3, state)
+
     # start_game("rQbqk2r/pp1ppppp/8/2pPB2P/1P1pP3/8/PPP3PP/R1n1K1NR w KQkq c6 3 6")
     # state = State("3k4/8/3p4/8/1p3P2/3B4/3P4/4K3 w - - 0 1")
-    state = State("rQb1k2r/pp1ppppp/8/2pPB2P/1P1pP3/6b1/PPP3PP/R3K2R w KQkq c6 3 6")
+    # state = State("rQb1k2r/pp1ppppp/8/2pPB2P/1P1pP3/6b1/PPP3PP/R3K2R w KQkq c6 3 6")
     # state.possible_actions = state.get_possible_actions(state.turn)
 
-    print("Moves")
-    for move in state.possible_actions: print(move)
+    # print("Moves")
+    # for move in state.possible_actions: print(move)
 
-    gui = GUI()
-    gui.run(state)
+    # gui = GUI()
+    # gui.run(state)
 
     # print(Fen.get_fen_from_state(state))
     # print(attack_actions)
